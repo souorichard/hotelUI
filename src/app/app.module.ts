485ab6bea478d57b6432d8d1b7032d05745b789d
unknown <elaine.ra96@gmail.com>
2022-06-08 11:00:19 -0300

    Removendo Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
-import { FooterComponent } from './sharepage/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
